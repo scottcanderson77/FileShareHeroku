4c8d4fa7115b35ea9b9562d50152d85c74e30830
vsc-squared <scottcanderson7@gmail.com>
2016-12-05 18:45:02 -0500

    updated

--- registration/views.py
+++ registration/views.py
@@ -61,7 +61,7 @@ def logout_page(request):
     return HttpResponseRedirect('/')
 
 
-@login_required
+@login_required#(redirect_field_name='login')
 def home(request):
     isSuperUser = False
     if request.user.is_superuser: isSuperUser = True
